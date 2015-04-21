9fd21cf5860563fdf0ac600258425b96d6da6559
cpcs499 <cpcs498.499@gmail.com>
2015-04-21 16:16:31 +0300

    first commit

---
+++ src/com/mzam/starter/Timeline.java
@@ -0,0 +1,33 @@
+package com.mzam.starter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class Timeline extends Activity {
+
+			
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.timeline);	
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}	
+}
